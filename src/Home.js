289425792c4fa026e144859d995ff30f60ef0290
Chim7a <80280391+Chim7a@users.noreply.github.com>
2022-04-19 18:27:03 +0100

    subtotal and checkout page component

--- src/Home.js
+++ src/Home.js
@@ -12,14 +12,14 @@ function Home() {
         
     <div className="home__row">
     <Product 
-        id="493898489123"
+        id="4938989779489123"
         title="A book you should'nt read."
         price={11.96}
         rating={4}
         image="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/action-thriller-book-cover-design-template-3675ae3e3ac7ee095fc793ab61b812cc_screen.jpg?ts=1637008457"
         />
     <Product 
-        id="12438723632233"
+        id="124878638723632233"
         title="Rolex watch"
         price={12622.22}
         rating={5}
@@ -29,21 +29,21 @@ function Home() {
 
     <div className="home__row">
     <Product 
-        id="143437823"
+        id="14386786437823"
         title="Apple Iphone 13"
         price={1244.96}
         rating={4}
         image="https://shop.ee.co.uk/medias/iphone-13-mini-5g-midnight-desktop-detail-1-Format-488?context=bWFzdGVyfHJvb3R8MTY5NTkyfGltYWdlL3BuZ3xzeXMtbWFzdGVyL3Jvb3QvaDEzL2g5Ni85NzI2NDE1ODk2NjA2L2lwaG9uZS0xMy1taW5pLTVnLW1pZG5pZ2h0LWRlc2t0b3AtZGV0YWlsLTFfRm9ybWF0LTQ4OHw3ODI4NjhmYjEzMTkwOTRiZDQ0MDQ3Y2Y4ZDhiNzQ0YTkyYmI0NzVmMWM1OGRkYjI4N2YwNDg1NjU4ODhkMWI3"
         />
     <Product 
-        id="128348733"
+        id="12834878786733"
         title="Lenovo M10 HD Grey 25.65 cm (10.1) 64 GB Tablet"
         price={111.96}
         rating={5}
         image="https://img.tatacliq.com/images/i7/1348Wx2000H/MP000000009695941_1348Wx2000H_202105312042442.jpeg"
         />
     <Product 
-        id="12767437643"
+        id="127674377677643"
         title="Amazon Alexa"
         price={110.96}
         rating={5}
@@ -53,7 +53,7 @@ function Home() {
 
     <div className="home__row">
     <Product 
-        id="45343123"
+        id="453437565123"
         title="LG 55UP7670PUC"
         price={119.96}
         rating={4}
--- src/Reducer.js
+++ src/Reducer.js
@@ -1,17 +1,54 @@
 export const initialState = {
-    basket: [],
+    basket: [
+        {id: "124878638723632233",
+        title: "Rolex watch",
+        price: 12622.22,
+        rating: 5,
+        image: "https://content.rolex.com/dam/watches/family-pages/datejust/update/2021/family-page-datejust-cover-m126333-0010_1601ac_003_portrait.jpg?imwidth=420",
+ },   
+        {id: "124878638723632233",
+        title: "Rolex watch",
+        price: 12622.22,
+        rating: 5,
+        image: "https://content.rolex.com/dam/watches/family-pages/datejust/update/2021/family-page-datejust-cover-m126333-0010_1601ac_003_portrait.jpg?imwidth=420",
+ },   
+
+],
+    user: null,
 };
+
+export const getBasketTotal = (basket) =>
+basket?.reduce((amount, item) => item.price + amount, 0);
 
 function reducer (state, action) {
     console.log(action);
     switch(action.type) {
         case 'ADD_TO_BASKET':
         // Logic for adding to basket.
+
             return {...state,
             basket: [...state.basket, action.item] 
         };
         case 'REMOVE_FROM_BASKET':
-            return {state};
+        // Logic for removing to basket.
+
+        // Cloned basket
+        let  newBasket = [...state.basket];
+
+        // if this product exist, finds an index(number), then do this.
+
+        const index = state.basket.findIndex((basketItem) => basketItem.id === action.id);
+
+        if (index >= 0) {
+            // item exists in basket, remove it
+            newBasket.splice(index, 1);
+        } else {
+            console.warn (
+                `cant remove product (id: ${action.id}) as its not in the basket`
+            );
+        }
+        // setting the state of basket to newBasket
+            return { ...state, basket: newBasket, };
         default:
         return state;
     }
